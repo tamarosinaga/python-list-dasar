e176a28062dc2b30bcde4c29ced3e34f98425aa0
bintang Tamaro <45529600+tamarosinaga@users.noreply.github.com>
2019-04-10 17:39:16 +0700

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,8 @@
+def Login():
+        print("\nlogin in system") ; user = input("username :") ;password =input("password :")
+        if user == "tamaro" and password == "sinaga1998":
+            pilih()
+        else :            
+            print (("\n         !!! WARNING !!!             "),
+                   ("\n!!! username atau password yang anda masukkan salah !!!"))
+            
